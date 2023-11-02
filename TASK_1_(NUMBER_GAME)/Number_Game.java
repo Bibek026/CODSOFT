5fff033a67ae53399b31cd512cd6b0bac57d6db4
Bibek Kr Maity <bibekkrmaity@gmail.com>
2023-11-02 23:19:37 +0530

    TASK_1 :- Number_Game , TAsk_2 :- Student_Grade_Calculator, Task_3 :- ATM_Interface

---
+++ TASK_1_(NUMBER_GAME)/Number_Game.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class Number_Game {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int totalAttempts = 0;
+        int rounds = 0;
+        int roundsWon = 0;
+        boolean playAgain = true;
+        final int MAX_ATTEMPTS = 10;//(int) (Math.random() * 10); // Maximum number of attempts allowed
+
+        while (playAgain) {
+            int myNumber = (int) (Math.random() * 100);
+            int attempts = 0;
+
+            do {
+                System.out.println("Guess my number: ");
+                int userNumber = sc.nextInt();
+                attempts++;
+
+                if (userNumber == myNumber) {
+                    System.out.println("WOHOO .. CORRECT NUMBER!!");
+                    roundsWon++; // Increment rounds won when the user guesses correctly
+                    break;
+                } else if (attempts >= MAX_ATTEMPTS) {
+                    System.out.println("Sorry, you've reached the maximum number of attempts. The correct number was: " + myNumber);
+                    break;
+                } else if (userNumber > myNumber) {
+                    System.out.println("Your number is too large.");
+                } else {
+                    System.out.println("Your number is too small.");
+                }
+            } while (true);
+
+            totalAttempts += attempts;
+            rounds++;
+
+            System.out.println("Number of attempts: " + attempts);
+            System.out.println("Do you want to play again? (yes/no): ");
+            String playAgainInput = sc.next().toLowerCase();
+            playAgain = playAgainInput.equals("yes");
+        }
+
+        double averageAttempts = (double) totalAttempts / rounds;
+
+        // Calculate the user's score
+        int userScore = roundsWon * 10 - (int) averageAttempts; // You can adjust the formula as needed
+
+        System.out.println("Thanks for playing!");
+        System.out.println("Total rounds played: " + rounds);
+        System.out.println("Rounds won: " + roundsWon);
+        System.out.println("Average atempts per round: " + averageAttempts);
+        System.out.println("User Score: " + userScore);
+        sc.close();
+    }
+}
---
+++ TASK_2_(STUDENT_GRADE_CALCULATOR)/Student_Grade_Calculator.java
@@ -0,0 +1,61 @@
+// Student_Grade_Calculator
+import java.util.Scanner;
+
+public class Student_Grade_Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+//input the nuber of subject
+        System.out.print("Enter the number of subjects: ");
+        int Sub = sc.nextInt();
+
+        int total_marks = 0;
+
+        // Input marks for each subject and calculate the total marks
+        for (int i = 0; i <Sub; i++) {
+            System.out.print("Enter marks (out of 100) for subject " + (i+1) + ": ");
+            int marks = sc.nextInt();
+            if (marks < 0 || marks > 100) {
+                System.out.println("Invalid mark");
+                i--;
+            }
+            else{
+                total_marks += marks;
+            }
+        }
+        
+
+        // Calculate the average percentage
+        double average = (double) total_marks/Sub;
+
+        // Calculate the Grade
+        String grade;
+
+        if(average >=90){
+            grade = "O";
+        }
+        else if(average >=80){
+            grade = "E";
+        }
+        else if(average >=70){
+            grade = "A";
+        }
+        else if(average >=60){
+            grade = "B"; 
+        }
+        else if(average >=50){
+            grade = "C";    
+        }
+         else if(average >=40){
+            grade = "D";
+         }
+        else{
+            grade = "F"; 
+        }
+        // Display the results
+        System.out.println("Total Marks of "+Sub+" Subjects:" + total_marks);
+        System.out.println("Average Percentage is: " + average+ "%");
+        System.out.println("Grade: " + grade);
+
+        sc.close();
+    }
+}
